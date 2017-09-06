87f9d7c07e5fc1c2949c69096b3cd2a2c4207ed0
COLIN <colindcli@users.noreply.github.com>
2017-09-06 10:27:15 +0800

    Create UnInstallService.bat

---
+++ Dotnet/WindowService/UnInstallService.bat
@@ -0,0 +1,3 @@
+cd /d C:\Users\HRD-007\Documents\Visual Studio 2017\Projects\ConsoleApp1\ConsoleApp1\bin\Release
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\InstallUtil /u ConsoleApp1.exe
+pause
