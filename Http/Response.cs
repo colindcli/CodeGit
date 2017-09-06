078d2e4c4899878add7176f389cd7a9909478228
COLIN <colindcli@users.noreply.github.com>
2017-09-06 11:56:51 +0800

    Update Response.cs

--- Http/Response.cs
+++ Http/Response.cs
@@ -10,21 +10,12 @@ public class Response
     /// <param name="fileName"></param>
     public static HttpResponseMessage ResponseFile(byte[] bt, string fileName)
     {
-        //var userAgent = System.Web.Http.Request.Headers.UserAgent.ToString();
-        //if (userAgent.IndexOf("AppleWebKit", StringComparison.OrdinalIgnoreCase) > -1)
-        //{
-        //    fileName = System.Web.HttpUtility.UrlEncode(fileName);
-        //}
-        //else if (userAgent.IndexOf("MSIE", StringComparison.OrdinalIgnoreCase) > -1)
-        //{
-        //    fileName = System.Web.HttpUtility.UrlEncode(fileName);
-        //}
-
         var response = new HttpResponseMessage { Content = new ByteArrayContent(bt) };
         response.Content.Headers.ContentLength = bt.Length;
         response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
         {
-            FileName = fileName
+            FileName = fileName,
+            FileNameStar = fileName //文件名乱码问题
         };
         response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
         return response;
