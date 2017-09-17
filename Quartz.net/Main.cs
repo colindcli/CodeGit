e8de6977ed5cb7591dd884a0ef5c4ec9793b4ac8
COLIN <colindcli@users.noreply.github.com>
2017-09-17 11:23:15 +0800

    Update Main.cs

--- Quartz.net/Main.cs
+++ Quartz.net/Main.cs
@@ -5,7 +5,7 @@ class Program
 {
     private static void Main(string[] args)
     {
-        TaskConfig.Run();
+        TaskConfig.RegisterQuartz();
 
         Console.WriteLine("Press any key to close the application");
         Console.ReadKey();
