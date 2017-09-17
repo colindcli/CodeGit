bb8514c7d80286a2f4785705bf6baead8591e767
COLIN <colindcli@users.noreply.github.com>
2017-09-17 11:08:28 +0800

    Update Main.cs

--- Quartz.net/Main.cs
+++ Quartz.net/Main.cs
@@ -5,26 +5,7 @@ class Program
 {
     private static void Main(string[] args)
     {
-        #region Job
-        Action<object, Exception> log = (message, ex) =>
-        {
-            Console.WriteLine(message);
-        };
-
-        var jobLists = new List<QuartzHelper.JobList>()
-        {
-            new QuartzHelper.JobList()
-            {
-                Name = "job",
-                Group = "group",
-                CronExpression = "0/5 * * * * ?",
-                JobType = new HelloJob()
-            }
-        };
-
-        var quartz = new QuartzHelper(log);
-        quartz.Run(jobLists);
-        #endregion
+        TaskConfig.Run();
 
         Console.WriteLine("Press any key to close the application");
         Console.ReadKey();
