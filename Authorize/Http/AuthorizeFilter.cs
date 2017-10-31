0096e8f5bde50e7fe8fb93e93320682030ecaf0f
COLIN <colindcli@users.noreply.github.com>
2017-10-31 10:07:38 +0800

    Create AuthorizeFilter.cs

---
+++ Authorize/Http/AuthorizeFilter.cs
@@ -0,0 +1,35 @@
+using System.Web.Http;
+
+/// <summary>
+/// 权限过滤
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeFilter : AuthorizeAttribute
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="actionContext"></param>
+    protected override void HandleUnauthorizedRequest(HttpActionContext actionContext)
+    {
+        base.HandleUnauthorizedRequest(actionContext);
+
+        //返回错误消息到前端
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="actionContext"></param>
+    /// <returns></returns>
+    protected override bool IsAuthorized(HttpActionContext actionContext)
+    {
+        var baseAuthor = base.IsAuthorized(actionContext);
+        if (!baseAuthor)
+            return false;
+
+        //获取用户内容
+
+        return true;
+    }
+}
