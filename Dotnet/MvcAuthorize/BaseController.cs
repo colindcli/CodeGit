97eb0a7fcae6c71ea8a212193c916e017295638f
COLIN <colindcli@users.noreply.github.com>
2017-11-27 22:15:25 +0800

    Update BaseController.cs

--- Dotnet/MvcAuthorize/BaseController.cs
+++ Dotnet/MvcAuthorize/BaseController.cs
@@ -4,20 +4,5 @@ using System.Web.Mvc;
 [AuthorizeFilter]
 public abstract class BaseController : Controller
 {
-    private int Id { get; set; }
-
-    protected int AdminId
-    {
-        get
-        {
-            if (Id > 0)
-            {
-                return Id;
-            }
-            var claims = HttpContext.User.Identity as ClaimsIdentity;
-            if (claims != null)
-                Id = int.Parse(claims.AuthenticationType);
-            return Id;
-        }
-    }
+    public int AdminId { get; set; }
 }
