255a05184c7ce213342bff425ac9a9d1158647f1
COLIN <colindcli@users.noreply.github.com>
2017-11-27 22:59:27 +0800

    Update and rename AuthorizeFilter.cs to AuthorizeApiFilter.cs

---
+++ Dotnet/ApiAuthorize/AuthorizeApiFilter.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+/// <summary>
+/// 权限过滤
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeApiFilter : AuthorizeAttribute
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="actionContext"></param>
+    /// <returns></returns>
+    protected override bool IsAuthorized(HttpActionContext actionContext)
+    {
+        base.IsAuthorized(actionContext);
+
+        var token = CookieHelper.GetCookie("Access_Token");
+        if (string.IsNullOrWhiteSpace(token))
+        {
+            return false;
+        }
+        var m = TokenHelper.GetModel<TokenModel>(token);
+        if (m == null)
+        {
+            return false;
+        }
+        if (m.ExpiryDate < DateTime.Now)
+        {
+            return false;
+        }
+        var controller = actionContext.ControllerContext.Controller as BaseApiController;
+        if (controller != null)
+            controller.AdminId = m.AdminId;
+        return true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="actionContext"></param>
+    protected override void HandleUnauthorizedRequest(HttpActionContext actionContext)
+    {
+        base.HandleUnauthorizedRequest(actionContext);
+
+        actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+        {
+            Content = new StringContent("没有授权")
+        };
+    }
+}
