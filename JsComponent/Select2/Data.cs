35979de8c9c08bd5682f9ee475098f15df8daea5
colindcli <978478090@qq.com>
2017-12-31 23:24:59 +0800

    select2

--- JsComponent/Select2/Data.cs
+++ JsComponent/Select2/Data.cs
@@ -1,3 +1,4 @@
+//普通数据结构
 public class Model
 {
     public List<result> results { get; set; }
@@ -6,6 +7,34 @@ public class Model
     {
         public Guid id { get; set; }
         public string text { get; set; }
+        public bool selected {get;set;}
+        public bool disabled {get;set;}
+    }
+
+    public class page
+    {
+        public bool more { get; set; }
+    }
+}
+
+
+//分组数据结构，https://select2.org/data-sources/formats
+public class Model
+{
+    public List<result> results { get; set; }
+    public page pagination { get; set; }
+    public class result
+    {
+        public string text { get; set; }
+        public List<Item> children {get;set;}
+    }
+
+    public class Item
+    {
+        public Guid id { get; set; }
+        public string text { get; set; }
+        public bool selected {get;set;}
+        public bool disabled {get;set;}
     }
 
     public class page
