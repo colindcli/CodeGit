f9a593e53b5a905e257242dcae3a2a5552a3d8f2
COLIN <colindcli@users.noreply.github.com>
2018-01-04 19:43:06 +0800

    Create DateVarchar.sql

---
+++ SqlServer/DateVarchar.sql
@@ -0,0 +1,26 @@
+DECLARE @dt DATETIME=getdate();
+DECLARE @Tb TABLE(Number int)
+DECLARE @i int=0;
+WHILE(@i<=14)
+	BEGIN
+	INSERT INTO @Tb SELECT @i;
+	SET @i=@i+1;
+	END;
+
+SET @i=20;
+WHILE(@i<=25)
+	BEGIN
+	INSERT INTO @Tb SELECT @i;
+	SET @i=@i+1;
+	END;
+
+SET @i=100;
+WHILE(@i<=114)
+	BEGIN
+	INSERT INTO @Tb SELECT @i;
+	SET @i=@i+1;
+	END;
+
+INSERT INTO @Tb SELECT 120 UNION SELECT 121 UNION SELECT 126 UNION SELECT 130;
+
+SELECT CONCAT('CONVERT(varchar(100), GETDATE(), ',t.Number,')') Keys,CONVERT(varchar(100), GETDATE(), t.Number) Value FROM @Tb t;
