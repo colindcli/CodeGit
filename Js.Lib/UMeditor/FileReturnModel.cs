0ef1f20e530065944f19614351dc35fc04265be7
COLIN <colindcli@users.noreply.github.com>
2018-02-21 14:24:00 +0800

    Create FileReturnModel.cs

---
+++ Js.Lib/UMeditor/FileReturnModel.cs
@@ -0,0 +1,9 @@
+public class EditorModel
+{
+    public string name { get; set; }
+    public string originalName { get; set; }
+    public long size { get; set; }
+    public string state { get; set; } = "SUCCESS";
+    public string type { get; set; }//.jpg
+    public string url { get; set; }
+}
