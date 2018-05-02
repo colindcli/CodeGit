48116034f9aff213145ee06860106cf24c1b4c60
colindcli <978478090@qq.com>
2018-05-02 14:23:15 +0800

    -

--- Net.Lib/Parser/ExCssHelper.cs
+++ Net.Lib/Parser/ExCssHelper.cs
@@ -3,23 +3,14 @@
 public class ExCssHelper
 {
     /// <summary>
-    /// 提取Css样式中的链接
-    /// Install-Package ExCSS -Version 2.0.6（最新版本是.Net Core）
+    /// 提取样式中的字体图标链接
     /// </summary>
-    /// <param name="cssDocument"></param>
-    /// <param name="isIncludeBase64Image">是否包含Base64类型图片</param>
+    /// <param name="stylesheet"></param>
     /// <returns></returns>
-    public static List<string> GetCssDocumentUrl(string cssDocument, bool isIncludeBase64Image)
+    public static List<string> GetCssDocumentFontUrl(StyleSheet stylesheet)
     {
-        var parser = new Parser();
-        var stylesheet = parser.Parse(cssDocument);
-
-        var paths = (from import in stylesheet.ImportDirectives
-            where import.RuleType == RuleType.Import && !string.IsNullOrWhiteSpace(import.Href)
-            select import.Href
-        ).ToList();
-
-        foreach (var stylesheetStyleRule in stylesheet.StyleRules)
+        var paths = new List<string>();
+        foreach (var stylesheetStyleRule in stylesheet.FontFaceDirectives)
         {
             foreach (var declaration in stylesheetStyleRule.Declarations)
             {
@@ -44,10 +35,67 @@ public class ExCssHelper
                 }
             }
         }
+
+        return paths;
+    }
+
+    /// <summary>
+    /// 提取样式中的背景链接
+    /// </summary>
+    /// <param name="stylesheet"></param>
+    /// <param name="isIncludeBase64Image">是否包含Base64类型图片</param>
+    /// <returns></returns>
+    public static List<string> GetCssDocumentBackgroupUrl(StyleSheet stylesheet, bool isIncludeBase64Image)
+    {
+        var paths = new List<string>();
+        foreach (var stylesheetStyleRule in stylesheet.StyleRules)
+        {
+            foreach (var declaration in stylesheetStyleRule.Declarations)
+            {
+                var declarationTerm = declaration.Term as PrimitiveTerm;
+                if (declarationTerm != null)
+                {
+                    var primitiveTerm = declarationTerm;
+                    if (primitiveTerm.PrimitiveType != UnitType.Uri)
+                        continue;
+                    var url = primitiveTerm.Value;
+                    paths.Add(url.ToString());
+                }
+                else if (declaration.Term is TermList)
+                {
+                    var termLists = (TermList)declaration.Term;
+                    paths.AddRange(from termList in termLists
+                                    select termList as PrimitiveTerm into term
+                                    select term into primitiveTerm
+                                    where primitiveTerm?.PrimitiveType == UnitType.Uri && primitiveTerm.Value != null
+                                    select primitiveTerm.Value into url
+                                    select url.ToString());
+                }
+            }
+        }
         if (isIncludeBase64Image)
         {
             return paths;
         }
         return paths.Where(p => !p.StartsWith("data:image/", StringComparison.OrdinalIgnoreCase)).ToList();
+    }
+
+    /// <summary>
+    /// 提取样式中的Import链接
+    /// </summary>
+    /// <param name="stylesheet"></param>
+    /// <param name="pageGuid"></param>
+    /// <param name="pageUrl"></param>
+    /// <returns></returns>
+    public static List<string> GetCssDocumentImport(StyleSheet stylesheet, Guid pageGuid, string pageUrl)
+    {
+        var urlUri = new Uri(pageUrl);
+
+        var paths = (from import in stylesheet.ImportDirectives
+                        where import.RuleType == RuleType.Import && !string.IsNullOrWhiteSpace(import.Href)
+                        select import.Href
+        ).ToList();
+
+        return paths;
     }
 }
