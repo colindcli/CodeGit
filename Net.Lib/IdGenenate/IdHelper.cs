9f5cac923e2747ce41f0d70866cc59d23a90da18
COLIN <colindcli@users.noreply.github.com>
2018-08-30 20:47:27 +0800

    -

--- Net.Lib/IdGenenate/IdHelper.cs
+++ Net.Lib/IdGenenate/IdHelper.cs
@@ -5,6 +5,11 @@ using System.Linq;
 
 public class IdHelper
 {
+    //41, 10, 12
+    //第一部分，1位为标识位，不用。
+    //第二部分，41位，用来记录当前时间与标记时间twepoch的毫秒数的差值，41位的时间截，可以使用69年，T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69
+    //第三部分，10位，用来记录当前节点的信息，支持2的10次方台机器
+    //第四部分，12位，用来支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号
     private static readonly IdGenerator Generator = new IdGenerator(0, new DateTime(2015, 1, 1, 0, 0, 0, DateTimeKind.Utc), new MaskConfig(45, 2, 16));
 
     /// <summary>
