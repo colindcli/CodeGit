6108d92ba4f552e0fc1edd0e311a8f45733c7552
COLIN <colindcli@users.noreply.github.com>
2018-09-03 14:55:46 +0800

    -

--- Js.Lib/PC-Mobile-Jump.js
+++ Js.Lib/PC-Mobile-Jump.js
@@ -1,4 +1,4 @@
-<!-- 手机、PC根据大小互跳 -->
+// 手机、PC根据大小互跳：判断移动端、判断pc端
 <script>
     var oWidth = document.documentElement.clientWidth || document.body.clientWidth;
     if (oWidth < 1200) {} else {
@@ -19,4 +19,16 @@
             window.location.href = "default.html";
         }
     }, false);
+</script>
+
+
+//https://github.com/nong0126/nong0126.github.com/blob/cc3a12525bee53a0bda5d8ffed1e733786569c3c/_site/html5/js/html5.js
+<script>
+(function (window) {
+    //判断移动端
+    var isMobi = false;
+    if (/(iPhone|iPad|iPod|android|windows phone os|iemobile)/i.test(navigator.userAgent)) {
+        isMobi = true;
+    }
+})();
 </script>
