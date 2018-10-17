4e08038bd59d16a46944ca8f9c77055ce131c6ab
COLIN <colindcli@users.noreply.github.com>
2018-10-17 16:15:19 +0800

    -

---
+++ Net.Lib/Queue/QueueHelper.cs
@@ -0,0 +1,67 @@
+using Priority_Queue;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 列队
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class QueueHelper<T>
+{
+    private static readonly SimplePriorityQueue<T> PriorityQueue = new SimplePriorityQueue<T>();
+
+    private static QueueHelper<T> Obj { get; set; }
+
+    public static QueueHelper<T> InitQueue(Action<T> action)
+    {
+        if (Obj != null)
+        {
+            return Obj;
+        }
+        Obj = new QueueHelper<T>();
+        Obj.Dequeue(action);
+        return Obj;
+    }
+
+    public void AddQueue(T item)
+    {
+        PriorityQueue.Enqueue(item, DateTime.Now.Ticks);
+    }
+
+    private void Dequeue(Action<T> action)
+    {
+        Task.Run(() =>
+        {
+            while (true)
+            {
+                while (PriorityQueue.Count != 0)
+                {
+                    try
+                    {
+                        var item = PriorityQueue.Dequeue();
+                        action?.Invoke(item);
+                    }
+                    catch (Exception)
+                    {
+                            // ignored
+                        }
+                }
+
+                Thread.Sleep(50);
+            }
+        });
+    }
+}
+
+/*
+//安装
+Install-Package OptimizedPriorityQueue -Version 4.1.1
+
+//调用
+var queue = QueueHelper<string>.InitQueue(m =>
+{
+    Console.WriteLine(m);
+});
+queue.AddQueue("test");
+*/
---
+++ Net.Lib/Queue/README.md
@@ -0,0 +1,7 @@
+## 列队
+
+
+> High-Speed-Priority-Queue-for-C-Sharp
+
+- [github](https://github.com/BlueRaja/High-Speed-Priority-Queue-for-C-Sharp)
+- [doc](https://github.com/BlueRaja/High-Speed-Priority-Queue-for-C-Sharp/wiki/Using-the-SimplePriorityQueue)
