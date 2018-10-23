610933a34b44ea01250b3705ea51aead7a784452
COLIN <colindcli@users.noreply.github.com>
2018-10-23 16:30:52 +0800

    -

--- Js.Lib/PC-Mobile-Jump.js
+++ Js.Lib/PC-Mobile-Jump.js
@@ -27,8 +27,15 @@
 (function (window) {
     //判断移动端（判断手机）
     var isMobi = false;
-    if (/(iPhone|iPad|iPod|android|windows phone os|iemobile)/i.test(navigator.userAgent)) {
-        isMobi = true;
+    if (/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i.test(navigator.userAgent)) {
+        isMobi = true
     }
+
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        alert('手机端')
+    }else{
+        alert('PC端')
+    }
+
 })();
 </script>
