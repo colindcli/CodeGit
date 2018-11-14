76c58d0d420c3ee1b8514b6fee550488cbfa4017
COLIN <colindcli@users.noreply.github.com>
2018-11-14 11:19:18 +0800

    -

--- Net.Utility/BlackIpHelper.cs
+++ Net.Utility/BlackIpHelper.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 /// <summary>
@@ -10,24 +11,31 @@ using System.Threading.Tasks;
 public class BlackIpHelper
 {
     private static readonly int Seconds = 60;
+    private static readonly Regex RegSpider = new Regex("Baiduspider|Googlebot|360Spider|Sosospider|sogou|bingbot|Yahoo|YoudaoBot", RegexOptions.IgnoreCase);
     private static readonly List<Ips> BlackIpLists = new List<Ips>();
     private static readonly List<Ips> IpLists = new List<Ips>();
 
     /// <summary>
-    /// 是否机器人Ip
+    /// 是否机器人IP
     /// </summary>
     /// <param name="ip"></param>
+    /// <param name="userAgent"></param>
     /// <returns></returns>
-    public static bool IsRobotIp(string ip)
+    public static bool IsRobotIp(string ip, string userAgent)
     {
         if (!Config.InterceptIp)
         {
             return false;
         }
 
-        if (string.IsNullOrWhiteSpace(ip))
+        if (string.IsNullOrWhiteSpace(ip) || string.IsNullOrWhiteSpace(userAgent))
         {
             return true;
+        }
+
+        if (RegSpider.IsMatch(userAgent))
+        {
+            return false;
         }
 
         if (BlackIpLists.Exists(p => p.Ip.Equals(ip)))
@@ -71,4 +79,15 @@ public class BlackIpHelper
         public string Ip { get; set; }
         public long Ticks { get; set; }
     }
+}
+
+
+
+//调用
+var ip = Request?.ServerVariables?.Get("REMOTE_ADDR");
+var userAgent = Request?.UserAgent;
+var b = BlackIpHelper.IsRobotIp(ip, userAgent);
+if (b)
+{
+    return Content($"他强由他强，清风拂山冈。他横任他横，明月照大江。", "text/html", Encoding.UTF8);
 }
