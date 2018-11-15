f7eab6d5b2bc4f32abd68a0d33501bab6e9ac3f2
COLIN <colindcli@users.noreply.github.com>
2018-11-15 15:13:51 +0800

    -

--- Net.Lib/Dapper.SimpleCRUD/RepositoryBase.cs
+++ Net.Lib/Dapper.SimpleCRUD/RepositoryBase.cs
@@ -1,16 +1,18 @@
 using Dapper;
+using ExecuteSqlBulk;
+using MyProject.Common;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Threading.Tasks;
 
 public abstract class RepositoryBase
 {
     private static readonly string ConnString = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ToString();
 
-    protected T Db<T>(Func<IDbConnection, T> func)
+    protected T Db<T>(Func<SqlConnection, T> func)
     {
         using (var conn = new SqlConnection(ConnString))
         {
@@ -22,7 +24,7 @@ public abstract class RepositoryBase
         }
     }
 
-    protected void Db(Action<IDbConnection> action)
+    protected void Db(Action<SqlConnection> action)
     {
         using (var conn = new SqlConnection(ConnString))
         {
@@ -33,7 +35,7 @@ public abstract class RepositoryBase
         }
     }
 
-    protected T Db<T>(Func<IDbConnection, IDbTransaction, T> func)
+    protected T Db<T>(Func<SqlConnection, SqlTransaction, T> func)
     {
         using (var conn = new SqlConnection(ConnString))
         {
@@ -57,7 +59,7 @@ public abstract class RepositoryBase
         }
     }
 
-    protected void Db(Action<IDbConnection, IDbTransaction> action)
+    protected void Db(Action<SqlConnection, SqlTransaction> action)
     {
         using (var conn = new SqlConnection(ConnString))
         {
@@ -80,10 +82,79 @@ public abstract class RepositoryBase
         }
     }
 
-    public List<T> GetList<T>()
+    #region 操作
+
+    public T Get<T>(object id)
     {
-        return Db(db => db.GetList<T>().ToList());
+        return Db(db => db.Get<T>(id));
     }
+
+    public Task<T> GetAsync<T>(object id)
+    {
+        return Task.Run(() => Db(db => db.Get<T>(id)));
+    }
+
+    public List<T> GetList<T>(object whereConditions = null)
+    {
+        return Db(db => db.GetList<T>(whereConditions).ToList());
+    }
+
+    public Task<IEnumerable<T>> GetListAsync<T>(object whereConditions = null)
+    {
+        return Task.Run(() => Db(db => db.GetList<T>(whereConditions)));
+    }
+
+    public int Update<T>(T m)
+    {
+        return Db(db => db.Update(m));
+    }
+
+    public Task<int> UpdateAsync<T>(T m)
+    {
+        return Task.Run(() => Db(db => db.Update(m)));
+    }
+
+    public int Delete<T>(T m)
+    {
+        return Db(db => db.Delete(m));
+    }
+
+    public Task<int> DeleteAsync<T>(T m)
+    {
+        return Task.Run(() => Db(db => db.Delete(m)));
+    }
+
+    public int DeleteList<T>(List<T> list)
+    {
+        return Db(db => db.DeleteList<T>(list));
+    }
+
+    public Task<int> DeleteListAsync<T>(List<T> list)
+    {
+        return Task.Run(() => Db(db => db.DeleteList<T>(list)));
+    }
+
+    public void BulkInsert<T>(List<T> list)
+    {
+        Db(db => db.BulkInsert(list));
+    }
+
+    public int BulkUpdate<T>(List<T> list, Func<T, object> columnUpdateExpression, Func<T, object> columnPrimaryKeyExpression) where T : new()
+    {
+        return Db(db => db.BulkUpdate(list, columnUpdateExpression, columnPrimaryKeyExpression));
+    }
+
+    public int BulkDelete<T>(List<T> list, Func<T, object> columnPrimaryKeyExpression) where T : new()
+    {
+        return Db(db => db.BulkDelete(list, columnPrimaryKeyExpression));
+    }
+
+    public void BulkDeleteAll<T>()
+    {
+        Db(db => db.BulkDelete<T>());
+    }
+
+    #endregion
 }
 
 public static class RepositoryExtension
