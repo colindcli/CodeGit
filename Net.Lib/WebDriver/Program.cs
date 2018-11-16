26d137f8d4b1403c1cb3457e77a686fce5d409d2
COLIN <colindcli@users.noreply.github.com>
2018-11-16 18:29:33 +0800

    -

---
+++ Net.Lib/WebDriver/Program.cs
@@ -0,0 +1,86 @@
+//https://www.nuget.org/packages/Selenium.Support/
+//https://www.nuget.org/packages/Selenium.WebDriver/
+//https://www.nuget.org/packages/Selenium.WebDriverBackedSelenium/
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Extensions;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("请输入网址：");
+            //var url = "https://blog.nuget.org/";
+            var url = Console.ReadLine()?.Replace("请输入网址：", "");
+
+            var guid = Guid.NewGuid().ToString();
+            var fileName = $"{AppDomain.CurrentDomain.BaseDirectory}{guid}";
+
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+
+            var heightStr = driver.ExecuteJavaScript<object>("return document.documentElement.scrollHeight+\"|\"+document.documentElement.clientHeight+\"|\"+document.documentElement.clientWidth").ToString();
+
+
+            var obj = heightStr.Split('|');
+            var scrollHeight = int.Parse(obj[0]);
+            var clientHeight = int.Parse(obj[1]);
+            var clientWidth = int.Parse(obj[2]);
+
+            var pageSize = scrollHeight / clientHeight;
+            int index = 0;
+            for (; index < pageSize; index++)
+            {
+                driver.ExecuteScript($"window.scrollTo(0,{clientHeight * index})");
+                driver.GetScreenshot().SaveAsFile($@"{fileName}{index}.png", ScreenshotImageFormat.Png);
+            }
+
+            if (scrollHeight % clientHeight > 0)
+            {
+                driver.ExecuteScript($"window.scrollTo(0,{scrollHeight})");
+                driver.GetScreenshot().SaveAsFile($@"{fileName}{index}.png", ScreenshotImageFormat.Png);
+            }
+
+
+            var bmp = new Bitmap(clientWidth - (scrollHeight % clientHeight > 0 ? 18 : 0), clientHeight * (index + 1));
+            var g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+
+            var i = 0;
+            for (; i < index; i++)
+            {
+                var img = Image.FromFile($@"{fileName}{i}.png");
+                g.DrawImage(img, 0, i * clientHeight, clientWidth, clientHeight);
+                img.Dispose();
+            }
+
+            if (scrollHeight % clientHeight > 0)
+            {
+                var img = Image.FromFile($@"{fileName}{i}.png");
+                g.DrawImage(img, 0, scrollHeight - clientHeight, clientWidth, clientHeight);
+                img.Dispose();
+            }
+
+            bmp.Save($@"{fileName}.png", ImageFormat.Png);
+
+            g.Dispose();
+            bmp.Dispose();
+
+
+            for (var j = 0; j <= i; j++)
+            {
+                File.Delete($@"{fileName}{j}.png");
+            }
+
+            Console.WriteLine(guid);
+        }
+    }
+}
---
+++ Net.Lib/WebDriver/README.md
@@ -0,0 +1,11 @@
+## WebDriver Net截图
+
+
+> 安装插件
+
+- https://www.nuget.org/packages/Selenium.Support/
+- https://www.nuget.org/packages/Selenium.WebDriver/
+- https://www.nuget.org/packages/Selenium.WebDriverBackedSelenium/
+
+
+> Program.cs
