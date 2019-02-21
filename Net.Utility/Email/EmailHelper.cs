e1f7f0b5aee40053646f70c03016861e294138db
COLIN <colindcli@users.noreply.github.com>
2019-02-21 15:26:06 +0800

    -

--- Net.Utility/Email/EmailHelper.cs
+++ Net.Utility/Email/EmailHelper.cs
@@ -1,18 +1,60 @@
+//https://www.nuget.org/packages/DwrUtility
+//Install-Package DwrUtility
 using System;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 
+/// <summary>
+/// 发送邮件
+/// </summary>
 public class EmailHelper
 {
-    public class SmtpClientModel
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SmtpModel
     {
+        /// <summary>
+        /// smtp服务，如：smtp.163.com
+        /// </summary>
         public string SmtpServerAddress { get; set; }
+        /// <summary>
+        /// smtp用户名：xxx@163.com
+        /// </summary>
         public string SmtpUserName { get; set; }
+        /// <summary>
+        /// smtp邮箱地址：xxx@163.com
+        /// </summary>
         public string FromMailAddress { get; set; }
+        /// <summary>
+        /// smtp密码
+        /// </summary>
         public string SmtpUserPassword { get; set; }
+        /// <summary>
+        /// 端口，默认25
+        /// </summary>
         public int SmtpServerPortNumber { get; set; } = 25;
+        /// <summary>
+        /// 是否启用安全套接字层，默认false
+        /// </summary>
         public bool EnableSsl { get; set; } = false;
+
+        /// <summary>
+        /// 日志(可选)
+        /// </summary>
+        public Action<Exception> Log { get; set; }
+    }
+
+    private SmtpModel Smtp { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="smtp"></param>
+    public EmailHelper(SmtpModel smtp)
+    {
+        Smtp = smtp;
     }
 
     /// <summary>
@@ -22,23 +64,22 @@ public class EmailHelper
     /// <param name="displayName">接收邮箱地址显示名称</param>
     /// <param name="subject">标题</param>
     /// <param name="body">内容</param>
-    /// <param name="smtpClient"></param>
     /// <returns></returns>
-    public static bool Send(string toEmail, string displayName, string subject, string body, SmtpClientModel smtpClient)
+    public bool Send(string toEmail, string displayName, string subject, string body)
     {
         var smtp = new SmtpClient
         {
             DeliveryMethod = SmtpDeliveryMethod.Network,
             UseDefaultCredentials = true,
-            Host = smtpClient.SmtpServerAddress,
-            Port = smtpClient.SmtpServerPortNumber,
-            Credentials = new NetworkCredential(smtpClient.SmtpUserName, smtpClient.SmtpUserPassword),
-            EnableSsl = smtpClient.EnableSsl,
+            Host = Smtp.SmtpServerAddress,
+            Port = Smtp.SmtpServerPortNumber,
+            Credentials = new NetworkCredential(Smtp.SmtpUserName, Smtp.SmtpUserPassword),
+            EnableSsl = Smtp.EnableSsl,
         };
 
         try
         {
-            var msg = new MailMessage(new MailAddress(smtpClient.FromMailAddress, displayName),
+            var msg = new MailMessage(new MailAddress(Smtp.FromMailAddress, displayName),
                 new MailAddress(toEmail))
             {
                 IsBodyHtml = true,
@@ -48,8 +89,9 @@ public class EmailHelper
             smtp.Send(msg);
             return true;
         }
-        catch (Exception)
+        catch (Exception ex)
         {
+            Smtp.Log?.Invoke(ex);
             return false;
         }
     }
@@ -61,10 +103,9 @@ public class EmailHelper
     /// <param name="displayName">接收邮箱地址显示名称</param>
     /// <param name="subject">标题</param>
     /// <param name="body">内容</param>
-    /// <param name="smtpClient"></param>
     /// <returns></returns>
-    public static Task<bool> SendAsynic(string toEmail, string displayName, string subject, string body, SmtpClientModel smtpClient)
+    public Task<bool> SendAsynic(string toEmail, string displayName, string subject, string body)
     {
-        return Task.Run(() => Send(toEmail, displayName, subject, body, smtpClient));
+        return Task.Run(() => Send(toEmail, displayName, subject, body));
     }
 }
