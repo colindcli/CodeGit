1f0aaddd7ed7a5bc521a1e24d57756269922a619
COLIN <colindcli@users.noreply.github.com>
2019-05-17 18:22:20 +0800

    -

--- Net.Utility/AssemblyHelper.cs
+++ Net.Utility/AssemblyHelper.cs
@@ -18,9 +18,9 @@ public class AssemblyHelper
     /// <summary>
     /// 调用对象的私有方法
     /// </summary>
-    /// <param name="instance"></param>
-    /// <param name="methodName"></param>
-    /// <param name="param"></param>
+    /// <param name="instance">对象</param>
+    /// <param name="methodName">方法名</param>
+    /// <param name="param">参数</param>
     /// <returns></returns>
     public static object CallObjectMethod<T>(T instance, string methodName, object[] param = null)
     {
@@ -31,11 +31,10 @@ public class AssemblyHelper
     /// <summary>
     /// 创建静态类型
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="assemblyString"></param>
-    /// <param name="namespaceClass"></param>
+    /// <param name="assemblyString">一般是Dll名称(不带后缀名)</param>
+    /// <param name="namespaceClass">命名空间.类名</param>
     /// <returns></returns>
-    public static Type CreateStaticClass<T>(string assemblyString, string namespaceClass)
+    public static Type CreateStaticClass(string assemblyString, string namespaceClass)
     {
         return Assembly.Load(assemblyString).GetType(namespaceClass);
     }
@@ -43,9 +42,9 @@ public class AssemblyHelper
     /// <summary>
     /// 调用类静态方法
     /// </summary>
-    /// <param name="type"></param>
-    /// <param name="methodName"></param>
-    /// <param name="param"></param>
+    /// <param name="type">类型</param>
+    /// <param name="methodName">方法名</param>
+    /// <param name="param">参数</param>
     /// <returns></returns>
     public static object CallStaticMethod(Type type, string methodName, object[] param = null)
     {
