f16b334be64543fd2f30c18f7deef1925d3e164b
colindcli <978478090@qq.com>
2019-06-22 21:08:00 +0800

    -

--- DataBase/SqlServer/CreateDatabase.sql
+++ DataBase/SqlServer/CreateDatabase.sql
@@ -1,19 +1,23 @@
+USE [master]
+GO
+
 --创建数据库
-CREATE DATABASE [Demo]
+CREATE DATABASE [Agent]
 ON PRIMARY
 	   (
-		   NAME=N'Demo',
-		   FILENAME=N'D:\Database\Demo.ndf',
+		   NAME=N'Agent',
+		   FILENAME=N'E:\Db2008\Agent.ndf',
 		   SIZE=5MB,
 		   MAXSIZE=UNLIMITED,
 		   FILEGROWTH=1024KB
 	   )
 LOG ON
 	(
-		NAME=N'Demo_log',
-		FILENAME=N'D:\Database\Demo_log.ldf',
+		NAME=N'Agent_log',
+		FILENAME=N'E:\Db2008\Agent_log.ldf',
 		SIZE=1MB,
 		MAXSIZE=UNLIMITED,
 		FILEGROWTH=10%
 	);
 GO
+
