5fb5e6fa119edee4b230a59d4ccdc2bbdd7ed93d
colindcli <978478090@qq.com>
2019-09-07 11:12:30 +0800

    -

--- Net.Utility/Xml/ConfigManage.cs
+++ Net.Utility/Xml/ConfigManage.cs
@@ -1,14 +1,19 @@
 //修改配置文件
 
 
-private static void ChangeConfiguration()
+/// <summary>
+/// 修改配置文件
+/// </summary>
+/// <param name="key"></param>
+/// <param name="value"></param>
+private static void ChangeAppSettings(string key, string value)
 {
     var assemblyConfigFile = Assembly.GetEntryAssembly()?.Location;
     var config = ConfigurationManager.OpenExeConfiguration(assemblyConfigFile);
     var appSettings = (AppSettingsSection)config.GetSection("appSettings");
 
-    appSettings.Settings.Remove("Version");
-    appSettings.Settings.Add("Version", DateTime.Now.ToString("HH:mm:ss"));
+    appSettings.Settings.Remove(key);
+    appSettings.Settings.Add(key, value);
 
     config.Save();
 }
